5050dbca2b86b330196643a3da60faf09649a89e
Kishan484 <121420677+Kishan484@users.noreply.github.com>
2025-02-06 08:41:00 +0530

    Update Redeem.py

--- plugins/Extra/Redeem.py
+++ plugins/Extra/Redeem.py
@@ -44,8 +44,8 @@ Aᴍᴏᴜɴᴛ:</b> {num_codes}
 
         keyboard = InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("♻️ Redeem Here ♻️", url="http://t.me/NehaTestBot")],
-                [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/IM_JISSHU")]
+                [InlineKeyboardButton("♻️ Redeem Here ♻️", url="https://t.me/MoviesSearchgroup_in")],
+                [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/OS_GOOD_LOVE")]
             ]
         )
 
